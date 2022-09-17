3c0ada20f91bd6c33c2522bbe9e10e0bf1b4e52e
Tiago Rodrigues <tiagoasrodrigues@protonmail.com>
2022-09-16 21:02:05 -0300

    :wrench: Setup project

--- App.tsx
+++ App.tsx
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text style={styles.button}>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+
+      <View>
+        <Button title='Send 1' />
+        <Button title='Send 2' />
+        <Button title='Send 3' />
+        <Button title='Send 4' />
+      </View>
     </View>
   );
 }
@@ -13,8 +33,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  title: {
+    color: 'red',
+    fontSize: 22,
+  },
+
+  button: {
+    color: 'white'
+  }
 });
